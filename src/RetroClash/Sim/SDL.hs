d778fda9650deaa457534f5b7de5a4666c5805c2
Dr. ERDI Gergo <gergo@erdi.hu>
2020-03-01 12:08:41 +0800

    Start reorganizing for clearer code

--- src/RetroClash/Sim/SDL.hs
+++ src/RetroClash/Sim/SDL.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RecordWildCards, ScopedTypeVariables, TypeApplications #-}
+{-# LANGUAGE RecordWildCards, ScopedTypeVariables, TupleSections #-}
 module RetroClash.Sim.SDL
     ( VideoParams(..)
     , withMainWindow
@@ -29,16 +29,70 @@ import Control.Monad
 import Control.Monad.IO.Class
 import Data.Array.IO
 import Data.IORef
+import Control.Monad.Loops (whileJust_)
+
+newtype Rasterizer (w :: Nat) (h :: Nat) = Rasterizer{ runRasterizer :: Ptr () -> Int -> IO () }
+
+data VideoParams = MkVideoParams
+    { windowTitle :: Text
+    , screenScale :: CInt
+    , screenRefreshRate :: Word32
+    }
+
+withMainWindow
+    :: forall w h m. (KnownNat w, KnownNat h, MonadIO m)
+    => VideoParams
+    -> ([Event] -> (Scancode -> Bool) -> m (Maybe (Rasterizer w h)))
+    -> m ()
+withMainWindow MkVideoParams{..} runFrame = do
+    initializeAll
+    window <- createWindow windowTitle defaultWindow
+    windowSize window $= fmap (screenScale *) screenSize
+
+    renderer <- createRenderer window (-1) defaultRenderer
+    texture <- createTexture renderer RGB888 TextureAccessStreaming screenSize
+
+    let withTexture drawTo = do
+            (ptr, rowstride) <- lockTexture texture Nothing
+            drawTo ptr (fromIntegral rowstride)
+            unlockTexture texture
+            SDL.copy renderer texture Nothing Nothing
+            present renderer
+
+    let render rasterizer = withTexture $ \ptr rowstride ->
+            liftIO $ runRasterizer rasterizer ptr rowstride
+
+    let prepareFrame = do
+            before <- ticks
+            events <- pollEvents
+            keys <- getKeyboardState
+            let windowClosed = any isWindowCloseEvent events
+            rasterizer <- if windowClosed then return Nothing else runFrame events keys
+            return $ (before,) <$> rasterizer
+
+        drawFrame (before, rasterizer) = do
+            render rasterizer
+            after <- ticks
+            let elapsed = after - before
+            when (elapsed < frameTime) $ liftIO $ threadDelay (fromIntegral (frameTime - elapsed) * 1000)
+
+    whileJust_ prepareFrame drawFrame
+  where
+    frameTime = 1000 `div` screenRefreshRate
+    screenSize = V2 (snatToNum (SNat @w)) (snatToNum (SNat @h))
+
+    isWindowCloseEvent ev = case eventPayload ev of
+        WindowClosedEvent{} -> True
+        _ -> False
 
 type Color = (Word8, Word8, Word8)
 
+{-# INLINE packColor #-}
 packColor :: Color -> Word32
 packColor (r, g, b) =
     fromIntegral r `shiftL` 16 .|.
     fromIntegral g `shiftL` 8 .|.
     fromIntegral b `shiftL` 0
-
-newtype Rasterizer (w :: Nat) (h :: Nat) = Rasterizer{ runRasterizer :: Ptr () -> Int -> IO () }
 
 rasterizePattern :: (KnownNat w, KnownNat h) => (Index w -> Index h -> Color) -> Rasterizer w h
 rasterizePattern draw = Rasterizer $ \ptr stride -> do
@@ -64,55 +118,3 @@ rasterizeBuffer (BufferArray arr) = Rasterizer $ \ptr stride -> do
             pokeElemOff base x =<< readArray arr (x, y)
   where
     ((x0, y0), (xn, yn)) = bufferBounds (SNat @w) (SNat @h)
-
-data VideoParams = MkVideoParams
-    { windowTitle :: Text
-    , screenScale :: CInt
-    , screenRefreshRate :: Word32
-    }
-
-withMainWindow
-    :: forall w h m. (KnownNat w, KnownNat h, MonadIO m)
-    => VideoParams
-    -> ([Event] -> (Scancode -> Bool) -> m (Maybe (Rasterizer w h)))
-    -> m ()
-withMainWindow MkVideoParams{..} runFrame = do
-    initializeAll
-    window <- createWindow windowTitle defaultWindow
-    windowSize window $= fmap (screenScale *) screenSize
-
-    renderer <- createRenderer window (-1) defaultRenderer
-    texture <- createTexture renderer RGB888 TextureAccessStreaming screenSize
-    return (window, renderer, texture)
-
-    let render rasterizer = do
-            (ptr, stride) <- lockTexture texture Nothing
-            liftIO $ runRasterizer rasterizer ptr (fromIntegral stride)
-            unlockTexture texture
-            SDL.copy renderer texture Nothing Nothing
-            present renderer
-
-    let loop = do
-            before <- ticks
-            events <- pollEvents
-            keys <- getKeyboardState
-            let windowClosed = any isQuitEvent events
-            draw <- if windowClosed then return Nothing else runFrame events keys
-            forM_ draw $ \draw -> do
-                render draw
-                after <- ticks
-                let elapsed = after - before
-                when (elapsed < frameTime) $ liftIO $ threadDelay (fromIntegral (frameTime - elapsed) * 1000)
-                loop
-    loop
-
-    destroyWindow window
-  where
-    frameTime = 1000 `div` screenRefreshRate
-    screenSize = V2 (snatToNum (SNat @w)) (snatToNum (SNat @h))
-
-    isQuitEvent ev = case eventPayload ev of
-        WindowClosedEvent{} -> True
-        KeyboardEvent KeyboardEventData{ keyboardEventKeysym = Keysym{..}, ..} ->
-            keyboardEventKeyMotion == Pressed && keysymKeycode == KeycodeEscape
-        _ -> False
